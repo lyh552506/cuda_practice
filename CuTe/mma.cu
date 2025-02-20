24c546b6064f2b2e6731a60bb31b6e8ffebc929c
lyh552506 <1661650252@qq.com>
2025-02-20 17:32:19 +0800

    [CuTe] simple gemm implemented by cute

--- CuTe/mma.cu
+++ CuTe/mma.cu
@@ -1,196 +1,89 @@
 #include <cuda.h>
+#include <mma.h>
 #include <stdlib.h>
 
+#include <iostream>
+
 #include "../3rdparty/cutlass/include/cute/tensor.hpp"
-
-#define PRINT(name, content) \
-  print(name);               \
-  print(" : ");              \
-  print(content);            \
-  print("\n");
-
-#define PRINTTENSOR(name, content) \
-  print(name);                     \
-  print(" : ");                    \
-  print_tensor(content);           \
-  print("\n");
-
-// #define PRINT_INFO
+#include "../utils/helper.hpp"
 using namespace cute;
 
-template <typename T>
-void gen_rand_data(T *data, int n);
-
-template <typename T, int kTileM, int kTileN, int kTileK, typename TiledMMA>
-__global__ void gemm_simple(T *Cptr, const T *Aptr, const T *Bptr, int m, int n,
-                            int k) {
-  Tensor A = make_tensor(make_gmem_ptr(Aptr), make_shape(m, k),
-                         make_stride(k, Int<1>{}));
-  Tensor B = make_tensor(make_gmem_ptr(Bptr), make_shape(n, k),
-                         make_stride(k, Int<1>{}));
-  Tensor C = make_tensor(make_gmem_ptr(Cptr), make_shape(m, n),
-                         make_stride(n, Int<1>{}));
-
-  int ix = blockIdx.x;
-  int iy = blockIdx.y;
-
-  //  gA(kTileM, kTileK, num_tile_k)
-  //  gB(kTileN, kTileK, num_tile_k)
-  //  gC(kTileM, kTileN)
+template <int BLOCK_M, int BLOCK_N, int BLOCK_K, typename TiledMMA>
+__global__ void CuTeGEMM(half *A, half *B, half *C, int M, int N, int K) {
   Tensor gA =
-      local_tile(A, make_tile(Int<kTileM>{}, Int<kTileK>{}), make_coord(iy, _));
+      make_tensor(make_gmem_ptr(A), make_shape(M, K), make_stride(K, Int<1>{}));
   Tensor gB =
-      local_tile(B, make_tile(Int<kTileN>{}, Int<kTileK>{}), make_coord(ix, _));
-  Tensor gC = local_tile(C, make_tile(Int<kTileM>{}, Int<kTileN>{}),
-                         make_coord(iy, ix));
+      make_tensor(make_gmem_ptr(B), make_shape(K, N), make_stride(N, Int<1>{}));
+  Tensor gC =
+      make_tensor(make_gmem_ptr(C), make_shape(M, N), make_stride(N, Int<1>{}));
 
-  TiledMMA tiled_mma;
-  auto thr_mma = tiled_mma.get_slice(threadIdx.x);
-  // MMA_M = M / (mma_op_m * thr_layout_m)
-  // MMA_N = N / (mma_op_n * thr_layout_n)
-  // MMA_K = K / (mma_op_k * thr_layout_k)
-  auto tAgA = thr_mma.partition_A(gA);  // (MMA, MMA_M, MMA_K, num_tile_k)
-  auto tBgB = thr_mma.partition_B(gB);  // (MMA, MMA_N, MMA_K, num_tile_k)
-  auto tCgC = thr_mma.partition_C(gC);  // (MMA, MMA_M, MMA_N)
+  Tensor load_gA = local_tile(gA, make_tile(Int<BLOCK_M>{}, Int<BLOCK_K>{}),
+                              make_coord(blockIdx.y, _));
+  Tensor load_gB = local_tile(gB, make_tile(Int<BLOCK_N>{}, Int<BLOCK_K>{}),
+                              make_coord(_, blockIdx.x));
+  Tensor load_gC = local_tile(gC, make_tile(Int<BLOCK_M>{}, Int<BLOCK_N>{}),
+                              make_coord(blockIdx.y, blockIdx.x));
 
-  // register tensor, shape is different with global memory tensor
-  auto tArA = thr_mma.partition_fragment_A(gA(_, _, 0));  // (MMA, MMA_M, MMA_K)
-  auto tBrB = thr_mma.partition_fragment_B(gB(_, _, 0));  // (MMA, MMA_N, MMA_K)
-  auto tCrC = thr_mma.partition_fragment_C(gC(_, _));     // (MMA, MMA_M, MMA_N)
-  clear(tCrC);
+  TiledMMA tiledmma;
+  ThrMMA thr_mma = tiledmma.get_thread_slice(threadIdx.x);
+  Tensor tAgA = thr_mma.partition_A(load_gA);
+  Tensor tBgB = thr_mma.partition_B(load_gB);
+  Tensor tCgC = thr_mma.partition_C(load_gC);
 
-  // #ifdef PRINT_INFO
-  /*
-      gA : (_128,_32,32)
-      tAgA : ((_2,_2,_2),_4,_2,32)
-      tArA : ((_2,_2,_2),_4,_2)
-      gB : (_128,_32,32)
-      tBgB : ((_2,_2),_8,_2,32)
-      tBrB : ((_2,_2),_8,_2)
-      gC : (_128,_128)
-      tCgC : ((_2,_2),_4,_8)
-      tCrC : ((_2,_2),_4,_8)
-  */
-  if (threadIdx.x == 0 && blockIdx.x == 0 && blockIdx.y == 0) {
-    PRINT("gA", gA.shape())
-    PRINT("tAgA", tAgA.shape())
-    PRINT("tArA", tArA.shape())
+  Tensor tArA = thr_mma.partition_fragment_A(gA(_, _, 0));
+  Tensor tBrB = thr_mma.partition_fragment_B(gB(_, _, 0));
+  Tensor tCrC = thr_mma.partition_fragment_C(gC(_, _));
 
-    PRINT("gB", gB.shape())
-    PRINT("tBgB", tBgB.shape())
-    PRINT("tBrB", tBrB.shape())
+  int slice_k = size<2>(load_gA);
+  for (int i = 0; i < slice_k; i++) {
+    copy(tAgA(_, _, _, i), tArA);
+    copy(tBgB(_, _, _, i), tBrB);
 
-    PRINT("gC", gC.shape())
-    PRINT("tCgC", tCgC.shape())
-    PRINT("tCrC", tCrC.shape())
+    gemm(tiledmma, tCrC, tArA, tBrB, tCrC);
   }
-  // #endif
-
-  int num_tile_k = size<2>(gA);
-#pragma unroll 1
-  for (int itile = 0; itile < num_tile_k; ++itile) {
-    // global memory to register
-    // just use cute::copy, not tiled
-    copy(tAgA(_, _, _, itile), tArA);
-    copy(tBgB(_, _, _, itile), tBrB);
-
-    // warp level, use  tiled_mma
-    gemm(tiled_mma, tCrC, tArA, tBrB, tCrC);
-  }
-  // register to global memory
   copy(tCrC, tCgC);
 }
 
 int main() {
-  srand(1000);
+  half *d_C, *d_A, *d_B, *A, *B, *C;
 
-  using T = cute::half_t;
-  cudaEvent_t start, end;
-  float elapsedTime;
-  cudaEventCreate(&start);
-  cudaEventCreate(&end);
+  constexpr int m = 1024 * 64;
+  constexpr int n = 128;
+  constexpr int k = 1024;
 
-  T *Cptr;
-  T *Aptr;
-  T *Bptr;
-
-  int m = 1024 * 64;
-  int n = 128;
-  int k = 1024;
-
-  //   cudaMalloc(&Cptr, sizeof(T) * m * n);
-  //   cudaMalloc(&Aptr, sizeof(T) * m * k);
-  //   cudaMalloc(&Bptr, sizeof(T) * k * n);
+  constexpr int block_m = 128;
+  constexpr int block_n = 64;
+  constexpr int block_k = 128;
   cudaError_t err;
-  err = cudaMalloc(&Cptr, sizeof(T) * m * n);
-  if (err != cudaSuccess) {
-    printf("cudaMalloc failed for Cptr: %s\n", cudaGetErrorString(err));
-    return -1;
-  }
-  err = cudaMalloc(&Aptr, sizeof(T) * m * k);
-  if (err != cudaSuccess) {
-    printf("cudaMalloc failed for Aptr: %s\n", cudaGetErrorString(err));
-    return -1;
-  }
-  err = cudaMalloc(&Bptr, sizeof(T) * k * n);
-  if (err != cudaSuccess) {
-    printf("cudaMalloc failed for Bptr: %s\n", cudaGetErrorString(err));
-    return -1;
-  }
+  checkCudaErrors(cudaMalloc(&d_C, sizeof(half) * m * n));
+  checkCudaErrors(cudaMalloc(&d_A, sizeof(half) * m * k));
+  checkCudaErrors(cudaMalloc(&d_B, sizeof(half) * k * n));
 
-  T *Aptr_host;
-  T *Bptr_host;
-  Aptr_host = (T *)malloc(sizeof(T) * m * k);
-  Bptr_host = (T *)malloc(sizeof(T) * n * k);
-  gen_rand_data(Aptr_host, m * k);
-  gen_rand_data(Bptr_host, n * k);
-  
-  err =cudaMemcpy(Aptr, Aptr_host, sizeof(T) * m * k, cudaMemcpyHostToDevice);
-  if (err != cudaSuccess) {
-    printf("cudaMalloc failed for Bptr: %s\n", cudaGetErrorString(err));
-    return -1;
-  }
-  err =cudaMemcpy(Bptr, Bptr_host, sizeof(T) * n * k, cudaMemcpyHostToDevice);
-  if (err != cudaSuccess) {
-    printf("cudaMalloc failed for Bptr: %s\n", cudaGetErrorString(err));
-    return -1;
-  }
-  // M=16,N=8,K=16
+  A = (half *)malloc(sizeof(half) * m * k);
+  B = (half *)malloc(sizeof(half) * n * k);
+  C = (half *)malloc(sizeof(half) * m * n);
+
+  helper::genRandomMatrix(A, m, k);
+  helper::genRandomMatrix(B, k, n);
+  helper::genEmptyMatrix(C, m, n);
+
+  checkCudaErrors(
+      cudaMemcpy(d_A, A, sizeof(half) * m * k, cudaMemcpyHostToDevice));
+  checkCudaErrors(
+      cudaMemcpy(d_B, B, sizeof(half) * n * k, cudaMemcpyHostToDevice));
+
   using mma_op = SM80_16x8x16_F16F16F16F16_TN;
   using mma_traits = MMA_Traits<mma_op>;
-  using mma_atom = MMA_Atom<mma_traits>;
-  using MMA = decltype(make_tiled_mma(
-      mma_atom{},
-      make_layout(Shape<_2, _2, _1>{}),    // the best shape for perf, use 2x2=4
-                                           // warps = 128 threads
-      make_layout(Shape<_1, _1, _1>{})));  // influence little to perf
+  using MMA_Atom = MMA_Atom<mma_traits>;
 
-  constexpr int kTileM = 128;
-  constexpr int kTileN = 128;
-  constexpr int kTileK = 32;
+  using TiledMMA = TiledMMA<MMA_Atom, Layout<Shape<Int<4>, _1, _1>>,
+                            Tile<Int<16 * 4>, _16, _16>>;
 
-  // each thread block handle with (kTileM, kTileN) output
-  dim3 grid(n / kTileN, m / kTileM);
-  dim3 block(size(MMA{}));
+  print(size(TiledMMA{}));
 
-  int count = 100;
-  cudaEventRecord(start);
-  //   for (int i = 0; i < count; ++i) {
-  gemm_simple<T, kTileM, kTileN, kTileK, MMA>
-      <<<grid, block>>>(Cptr, Aptr, Bptr, m, n, k);
-  //   }
-  err = cudaGetLastError();
-  printf("err = %d, str = %s\n", err, cudaGetErrorString(err));
-  cudaEventRecord(end);
-  cudaEventSynchronize(end);
-  cudaEventElapsedTime(&elapsedTime, start, end);
-  std::cout << "gemm-simple took " << elapsedTime / count << "ms." << std::endl;
-}
+  dim3 Grid(n / block_n, m / block_m);
+  dim3 Block(size(TiledMMA{}));
 
-template <typename T>
-void gen_rand_data(T *data, int n) {
-  for (int i = 0; i < n; ++i) {
-    float v = (rand() % 200 - 100) * 0.01;
-    data[i] = v;
-  }
+  CuTeGEMM<block_m, block_n, block_k, TiledMMA>
+      <<<Grid, Block>>>(d_A, d_B, d_C, m, n, k);
 }
--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 THUNDERKITTENS_ROOT:=/root/autodl-tmp/cuda_practice/3rdparty/ThunderKittens
 CuTe_ROOT:=/root/autodl-tmp/cuda_practice/3rdparty/cutlass/include
+NVCCFLAGS=-DNDEBUG -Xcompiler=-fPIE --expt-extended-lambda --expt-relaxed-constexpr -Xcompiler=-Wno-psabi -Xcompiler=-fno-strict-aliasing --use_fast_math -forward-unknown-to-host-compiler -g -G -Xnvlink=--verbose -Xptxas=--verbose -Xptxas=--warn-on-spills -MD -MT -MF -x cu -lrt -lpthread -ldl -lcuda
 # ARCH:=-DKITTENS_HOPPER
 ARCH:=-DKITTENS_4090
 run:
 	@echo $(path)
-	nvcc -std=c++20 -g -G -o target $(path) --expt-relaxed-constexpr -lrt -lpthread -ldl ${ARCH} -arch=sm_89 -lcuda -lcudadevrt -lcudart_static -lcublas -I${CuTe_ROOT} -I${THUNDERKITTENS_ROOT}/include -I${THUNDERKITTENS_ROOT}/prototype 
+	nvcc -std=c++20 -g -G -o target $(path) ${ARCH} -arch=sm_89 $(NVCCFLAGS) -I${CuTe_ROOT} -I${THUNDERKITTENS_ROOT}/include -I${THUNDERKITTENS_ROOT}/prototype 
 
 	
